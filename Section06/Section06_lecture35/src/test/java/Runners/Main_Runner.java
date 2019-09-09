7844c079302baeff96491b08c4dea4cd650ad858
Louis <luismedinacoca@gmail.com>
2019-09-09 18:02:35 -0300

    last lecture modified

---
+++ Section06/Section06_lecture35/src/test/java/Runners/Main_Runner.java
@@ -0,0 +1,19 @@
+package Runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin={"pretty",
+                "json:target\\cucumber_json_reports\\home-page.json",
+                "html:target\\home-page-html",
+                "com.cucumber.listener.ExtentCucumberFormatter:target/report.html"},
+        features={"D:\\Programming\\[Cucumber]\\Gianni Bruno - Cucumber Selenium Java\\Section06\\Section06_lecture35\\src\\test\\java\\Features"},
+        glue={"Step_Definitions"},
+        monochrome = true
+)
+public class Main_Runner {
+
+}
